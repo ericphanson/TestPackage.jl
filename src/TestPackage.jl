8352e11e7cdcf5d835a116f72769f9d26d5efb08
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2021-06-07 11:21:55 +0200

    Mini test

--- src/TestPackage.jl
+++ src/TestPackage.jl
@@ -2,4 +2,12 @@ module TestPackage
 
 # Write your package code here.
 
+using LinearAlgebra
+
+function addition(a, b)
+    return a+b
+end
+
+export addition, eigvals
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "TestPackage.jl" begin
     # Write your tests here.
+    @test addition(1, 2) == 3
+    @test eigvals(ones(2, 2)) == [0., 2.]
 end
